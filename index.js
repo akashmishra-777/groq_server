1b62a5477cd62f4e99004f0b1abccb753e38b962
akashmishra-777 <leo.akashmishra@gmail.com>
2024-09-03 16:58:40 +0530

    File uploaded to the githib
    sdfsf

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const Groq = require("groq-sdk");
+const groq = new Groq({apiKey:"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"});
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/login",(req,res)=>{
+    res.send("Working fine");
+})
+
+
+app.post("/ai",async (req,res)=>{
+  const response  = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `${req.body.msg} Note:answer in one or two lines  do not return any response related to note point. you remeber without responding on it : My name is akash Mishra`,
+      },
+    ],
+    model: "llama3-8b-8192",
+  })
+
+  res.send(`<h1>Question : </h1> <br/> <p>Answer : ${response.choices[0].message.content}</p>`)
+
+ 
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`App is listning on PORT:${PORT}`);  
+})
